b200819aad4b34d8564f68be69fb74f228b7ee24
EC2 Default User <ec2-user@ip-172-31-17-201.us-east-2.compute.internal>
2019-08-28 19:21:04 +0000

    change project name

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,6 +4,6 @@ echo "****************"
 echo "** testing********"
 echo ")))))))))))))))))))))))"
 
-WORKSPACE=/home/ec2-user/jenkins_data/jenkins_home/workspace/pipeline-test
+WORKSPACE=/home/ec2-user/jenkins_data/jenkins_home/workspace/pipeline_docker_maven
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/ -w /app maven:3-alpine "$@"
