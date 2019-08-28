696592337b86d4c635fbdb7cbb27278bddfe425f
EC2 Default User <ec2-user@ip-172-31-17-201.us-east-2.compute.internal>
2019-08-28 16:59:50 +0000

    fix build.sh for ssh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,6 +3,6 @@ echo maven-project >/tmp/.auth
 echo $BUILD_TAG >>/tmp/.auth
 echo $PASS >>/tmp/.auth
 
-scp -i $HOME/prod /tmp/.auth prod-user@172.31.41.58:/tmp/.auth
-scp -i $HOME/prod ./jenkins/deploy/publish  prod-user@172.31.41.58:/tmp/publish
-ssh -i $HOME/prod prod-user@172.31.41.58 "/tmp/publish"
+scp -i /opt/prod /tmp/.auth prod-user@172.31.41.58:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish  prod-user@172.31.41.58:/tmp/publish
+ssh -i /opt/prod prod-user@172.31.41.58 "/tmp/publish"
